0d7c20ee85f2062bce0f0acc05c3f24bd9a831ad
sSahli232 <sahli.selim@gmail.com>
2025-07-22 19:55:34 +0200

    feat(add-patient): Create zod validation pipe

---
+++ backend/src/pipes/zod-validation.pipe.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
+import { z } from 'zod';
+
+@Injectable()
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schema: z.Schema<any>) {}
+
+  transform(payload: any) {
+    const result = this.schema.safeParse(payload);
+
+    if (result.success === true) {
+      return result.data;
+    }
+
+    throw new BadRequestException('Failed to validate');
+  }
+}
