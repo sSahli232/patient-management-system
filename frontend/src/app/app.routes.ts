7317a5b5b8531195aaa70a7e017695926c77cdba
sSahli232 <sahli.selim@gmail.com>
2025-07-23 23:38:08 +0200

    feat: Always redirect to patients page when the user is logged in

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,8 +2,15 @@ import { Routes } from '@angular/router';
 import { Auth } from './core/auth/auth';
 import { isUserAuthenticated } from './core/auth/guards/auth.guard';
 import { Patients } from './features/patients/patients';
+import { inject } from '@angular/core';
+import { AuthService } from './core/auth/services/auth.service';
 
 export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: () => inject(AuthService).isLoggedIn() ? 'patients' : 'login',
+    pathMatch: 'full',
+  },
   {
     path: "login",
     component: Auth, // TODO: redirect to patient page when the user is already logged in.
