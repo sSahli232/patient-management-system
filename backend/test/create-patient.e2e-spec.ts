9c12924a90d3dff4ec4bc757264dee380880da52
sSahli232 <sahli.selim@gmail.com>
2025-07-23 07:19:35 +0200

    refactor: Improve test name

--- backend/test/create-patient.e2e-spec.ts
+++ backend/test/create-patient.e2e-spec.ts
@@ -20,33 +20,36 @@ describe('Feature: creating a patient', () => {
     await app.cleanup();
   });
 
-  it('should create a patient', async () => {
-    const result = await request(app.getHttpServer())
-      .post('/patients')
-      .send({
+  describe('Scenario: Happy path', () => {
+    it('should create a patient', async () => {
+      const result = await request(app.getHttpServer())
+        .post('/patients')
+        .send({
+          firstName: 'John',
+          lastName: 'Doe',
+          email: 'johndoe@gmail.com',
+          phoneNumber: '+33102030405',
+          dateOfBirth: new Date('1980-03-01T00:00:00.000Z'),
+        });
+
+      expect(result.status).toBe(201);
+      expect(result.body).toEqual({
+        id: expect.any(String),
+      });
+
+      const patientRepository =
+        app.get<IPatientRepository>(I_PATIENT_REPOSITORY);
+      const patient = await patientRepository.findById(result.body.id);
+
+      expect(patient).toBeDefined();
+      expect(patient?.props).toEqual({
+        id: result.body.id,
         firstName: 'John',
         lastName: 'Doe',
         email: 'johndoe@gmail.com',
         phoneNumber: '+33102030405',
-        dateOfBirth: new Date('1980-03-01T00:00:00.000Z'),
+        dateOfBirth: '1980-03-01T00:00:00.000Z',
       });
-
-    expect(result.status).toBe(201);
-    expect(result.body).toEqual({
-      id: expect.any(String),
-    });
-
-    const patientRepository = app.get<IPatientRepository>(I_PATIENT_REPOSITORY);
-    const patient = await patientRepository.findById(result.body.id);
-
-    expect(patient).toBeDefined();
-    expect(patient?.props).toEqual({
-      id: result.body.id,
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'johndoe@gmail.com',
-      phoneNumber: '+33102030405',
-      dateOfBirth: '1980-03-01T00:00:00.000Z',
     });
   });
 });
