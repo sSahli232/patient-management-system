92f7b760862ce0d33e686046212e19cf8b579288
sSahli232 <sahli.selim@gmail.com>
2025-07-23 16:09:10 +0200

    feat: Hard coded an admin user for testing purpose

--- backend/src/users/adapters/in-memory-user-repository.ts
+++ backend/src/users/adapters/in-memory-user-repository.ts
@@ -1,8 +1,16 @@
 import { User } from '../entities/user.entity';
 import { IUserRepository } from '../ports/user-repository.interface';
 
+const rootAdmin = new User({
+  id: 'admin-id',
+  email: 'admin@mail.com',
+  password: '$2b$10$WzOryozGBpphYwI3NS1/9.JsXt/fGOIi/85d7PZhLFRnRq24bznfe',
+  roles: ['admin'],
+});
 export class InMemoryUserRepository implements IUserRepository {
-  constructor(private readonly database: User[] = []) {}
+  constructor(private readonly database: User[] = []) {
+    this.database.push(rootAdmin);
+  }
 
   async create(user: User): Promise<void> {
     this.database.push(user);
